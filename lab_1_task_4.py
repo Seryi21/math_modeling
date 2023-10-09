0367922e56c989ae3b4627868593b68c3355f543
Seryi21 <144707354+Seryi21@users.noreply.github.com>
2023-10-09 18:41:50 +0300

    1

---
+++ lab_1_task_4.py
@@ -0,0 +1,7 @@
+a = [1, 5, 'Good', 'Bad']
+b = [9, 'Blue', 'Red', 11]
+print(str(a[0]) + b[2])
+print(a[1] + a[1])
+print(a[0] * b[0])
+print(a[0]**b[0])
+print(a + b)
---
+++ lec_1_list.py
@@ -0,0 +1,15 @@
+a = [1, 2, 3]
+print(a[0])
+
+b = [8, 10, 11]
+
+c = a + b 
+print(c)
+
+print(c * 2)
+
+c.append ('good')
+print(c)
+
+a.append(b)
+print(a)
