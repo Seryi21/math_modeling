12250aff3ffbcf197fa2c54db490b8db3f4e1bd5
Seryi21 <144707354+Seryi21@users.noreply.github.com>
2023-11-20 17:57:43 +0300

    1

---
+++ lab_3_task_1.py
@@ -0,0 +1,5 @@
+gravitational_constant = 6.67 * 10**(-11)
+speed_of_light = 3 * 10**8
+acceleration_of_free_fall = 9.8
+pi = 3.14
+avogadro_number = 2.24 * 10**(-2)
---
+++ lec_3_from_as.py
@@ -0,0 +1,4 @@
+from lec_3_my_module import earth_mass as em
+from lec_3_my_module import gravity_constant as G
+
+g = 500 * G / 10**2
